92f406d1374dc30aeb3c8f6dbe8a7b820367e476
Sargenk <Sargenbk@alfredstate.edu>
2022-01-09 15:56:58 -0500

    added servicerestarter.sh (requires user to enter services and copy error checker

--- linux/serviceRestarter.sh
+++ linux/serviceRestarter.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
+if [[ 'id -u' -eq 0 ]]
+then
+
+else
+	echo "sudo privileges required"
+	exit 1
+
+fi
+
 clear
 Green='\033[0;32m'
 NC='\033[0m'
 Red='\033[0;31m'
 while true;
 do
-  sudo systemctl start sshdd
   #sudo systemctl start <serviceName>
+
+  if [[ $? -eq 0 ]]; then
+        echo -e "Service ${Green}OK${NC} $(date +"%I:%M:%S %p")"
+  else
+        echo -e "${Red}Error${NC}  $(date +"%I:%M:%S %p")"
+  fi
+
+  #sudo systemctl start <serviceName>
+
   if [[ $? -eq 0 ]]; then
   	echo -e "Service ${Green}OK${NC} $(date +"%I:%M:%S %p")"
   else
   	echo -e "${Red}Error${NC}  $(date +"%I:%M:%S %p")"
   fi
+
   sleep 3s
 done
