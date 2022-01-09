9ab2193c5e832d41ef02be5c51a5f23a92cae8f7
Sargenk <Sargenbk@alfredstate.edu>
2022-01-09 16:04:23 -0500

    made new user adder hide entered password and require reentered password

--- linux/newUserAdder.sh
+++ linux/newUserAdder.sh
@@ -6,7 +6,15 @@ fi
 
 echo "please enter new password"​
 
-read passwd​
+read -s -p passwd​
+
+echo "please confirm password"
+
+read -s -p cpasswd
+
+if [[ passwd -eq cpasswd ]]
+
+then
 
 useradd  –m -s /bin/bash -p $(echo $passwd | openssl passwd -1 -stdin) potato​
 
@@ -15,4 +23,12 @@ echo "adding remote login detector"​
 echo "wall someone logged into the $USER account from $SSH_CONNECTION" >> /home/potato/.bashrc​
 
 echo "user potato made with given password"
-exit
+
+exit 0
+else
+
+echo "passwords do not match. please restart program"
+
+exit 1
+
+fi
