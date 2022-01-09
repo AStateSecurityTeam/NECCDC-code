44224f8ba24645dd12a5006736e6a76aa3020723
Sargenk <Sargenbk@alfredstate.edu>
2022-01-09 18:45:30 -0500

    modified remotelogin detector script to working condition

--- linux/remoteLoginDetector.sh
+++ linux/remoteLoginDetector.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ 'id -u' -eq 0];
-then
-	echo "adding remote login detector to spudnik and root account"
 
-        echo "wall someone logged into the $USER account from $SSH_CONNECTION" >> /home/spudnik/.bashrc
-	echo "wall someone logged into the Root account from $SSH_CONNECTION" >> /root/.bashrc
-	exit 0
-else
-	echo "please run this program in sudo mode"
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+	echo "this program must be run in root"
 	exit 1
 fi
+	echo "adding remote login detector to spudnik and root account"
+
+        echo "wall someone logged into the Spudnik account from \$SSH_CONNECTION" >> /home/spudnik/.bashrc
+	echo "wall someone logged into the Root account from \$SSH_CONNECTION" >> /root/.bashrc
+	exit 0
